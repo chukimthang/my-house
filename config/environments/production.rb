064007c79472e11548da865447c0e765d5931dc2
Chu Kim Thang <chukimthang94@gmail.com>
2025-04-12 13:25:41 +0700

    Change host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.hosts << "my-house-production.up.railway.app"
+  config.hosts << "my-house.up.railway.app"
 end
