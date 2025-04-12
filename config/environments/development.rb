dd26ad700c80eaff6ffc39e3f353dd6186bc73d9
Chu Kim Thang <chukimthang94@gmail.com>
2025-04-12 11:55:47 +0700

    Change host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,5 +69,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.hosts << "web-production-9c4e.up.railway.app"
+  config.hosts << "my-house-production.up.railway.app"
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.hosts << "web-production-9c4e.up.railway.app"
+  config.hosts << "my-house-production.up.railway.app"
 end
