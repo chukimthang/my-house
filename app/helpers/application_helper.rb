f9ae080b2e628d96564bef00c664026d5a6a5154
Chu Kim Thang <chukimthang94@gmail.com>
2025-04-19 23:15:37 +0700

    Change format

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def format_money(amount)
-    number_to_currency(amount, precision: 0, unit: 'VND', format: "%n %u")
+    number_to_currency(amount, precision: 0, unit: 'đ', format: "%n %u")
   end
 end
